a1d648cf0d0a2e8d72c04ec2e3a69b9eaddab3f0
unisbadri <unis.badri@elementcreativestudio.com>
2021-04-08 14:18:34 +0700

    change server to listen to 0.0.0.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    host: '0.0.0.0'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxtjsdockerbuild',
